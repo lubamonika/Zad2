f17b389e24ece3db50755fdae4139b42e535c005
Monika Luba <lubamonika@gmail.com>
2018-06-08 16:57:38 +0200

    wersja rozgrzebana z klasami

--- src/com/infoshareacademy/Dzialania.java
+++ src/com/infoshareacademy/Dzialania.java
@@ -5,12 +5,33 @@ public class Dzialania {
     public static int dodawanie(int[] liczby) {
         int ile = liczby.length;
         int wynikDodawania = liczby[0];
-        for (int i=1; i<ile; i++){
+        for (int i = 1; i < ile; i++) {
             wynikDodawania += liczby[i];
         }
-        System.out.println(wynikDodawania);
-        System.out.println("dodawanie");
+        String jaktotambylo = "1+2+3";
+        wypiszWynik("dodawanie",jaktotambylo , wynikDodawania);
         return wynikDodawania;
-
     }
+
+    public static int odejmowanie(int[] liczby) {
+        int ile = liczby.length;
+        int wynikOdejmowania = liczby[0];
+        for (int i = 1; i < ile; i++) {
+            wynikOdejmowania += liczby[i];
+        }
+        String jaktotambylo = "1-2-3";
+        wypiszWynik("odejmowanie",jaktotambylo , wynikOdejmowania);
+        return wynikOdejmowania;
+    }
+
+    private static void wypiszWynik(String nazwaOperacji, String opearacjaPisemnie, int wynik) {
+        StringBuilder sb= new StringBuilder();
+        sb.append("Operacja:").append(nazwaOperacji);
+        sb.append("Po kolei :").append(opearacjaPisemnie);
+        sb.append("wYNIK :").append(wynik);
+
+        System.out.println(sb.toString());
+    }
+
 }
+
--- src/com/infoshareacademy/Main.java
+++ src/com/infoshareacademy/Main.java
@@ -8,16 +8,13 @@ public class Main {
 
 
     public static void main(String[] args) {
-
         System.out.println("Możliwe działania:");
         Operacja ope[] = Operacja.values();
         int size = Operacja.values().length;
-        for (int i=0; i<size; i++) {
-            System.out.println((i+1) + ": " + ope[i]);
+        for (int i = 0; i < size; i++) {
+            System.out.println((i + 1) + ": " + ope[i]);
         }
         System.out.println(" ");
-
-
 
         System.out.println("Wpisz opcję (1-4) z klawiatury:");
         Scanner scanner = new Scanner(System.in);
@@ -28,49 +25,45 @@ public class Main {
         int ile = scanner.nextInt();
         //System.out.println(ile);
         int[] liczby = new int[ile];
-        String[] outputArray = new String[ile];
+        //String[] outputArray = new String[ile];
 
-        for (int i=0; i<ile; i++) {
-        System.out.println("Podaj liczbę " + (i+1));
-        liczby[i] = scanner.nextInt();
+        for (int i = 0; i < ile; i++) {
+            System.out.println("Podaj liczbę " + (i + 1));
+            liczby[i] = scanner.nextInt();
         }
 
-        for (int i=0; i<ile; i++) {
-            outputArray[i] = String.valueOf(liczby[i]);
-        }
+        //for (int i=0; i<ile; i++) {
+        //    outputArray[i] = String.valueOf(liczby[i]);
+        //}
 
-        String output = "Operacja : " + outputArray[0];
+        String output = "Operacja : " + liczby[0];
         int wynik = liczby[0];
 
         switch (dzialanie) {
             case 1:
-                for (int i=1; i<ile; i++){
-                   wynik += liczby[i];
-                   output = output+" + "+outputArray[i];
-                }
-                System.out.println(output);
-                System.out.println("Wynik: " + wynik);
+                Dzialania.dodawanie(liczby);
+
                 break;
             case 2:
-                for (int i=1; i<ile; i++){
+                for (int i = 1; i < ile; i++) {
                     wynik -= liczby[i];
-                    output = output+" - "+outputArray[i];
+                    output = output + " - " + liczby[i];
                 }
                 System.out.println(output);
                 System.out.println("Wynik :" + wynik);
                 break;
             case 3:
-                for (int i=1; i<ile; i++){
+                for (int i = 1; i < ile; i++) {
                     wynik *= liczby[i];
-                    output = output+" * "+outputArray[i];
+                    output = output + " * " + liczby[i];
                 }
                 System.out.println(output);
                 System.out.println("Wynik :" + wynik);
                 break;
             case 4:
-                for (int i=1; i<ile; i++){
+                for (int i = 1; i < ile; i++) {
                     wynik /= liczby[i];
-                    output = output+" / "+outputArray[i];
+                    output = output + " / " + liczby[i];
                 }
                 System.out.println(output);
                 System.out.println("Wynik :" + wynik);
@@ -79,6 +72,45 @@ public class Main {
                 System.out.println("Nie podałeś w pierwszym polu liczby z przedziału 1-4");
 
         }
+//
+//        switch (dzialanie) {
+//            case 1:
+//                //Dzialania.dodawanie(liczby);
+//                for (int i = 1; i < ile; i++) {
+//                    wynik += liczby[i];
+//                    output = output + " + " + liczby[i];
+//                }
+//                System.out.println(output);
+//                System.out.println("Wynik: " + wynik);
+//                break;
+//            case 2:
+//                for (int i = 1; i < ile; i++) {
+//                    wynik -= liczby[i];
+//                    output = output + " - " + liczby[i];
+//                }
+//                System.out.println(output);
+//                System.out.println("Wynik :" + wynik);
+//                break;
+//            case 3:
+//                for (int i = 1; i < ile; i++) {
+//                    wynik *= liczby[i];
+//                    output = output + " * " + liczby[i];
+//                }
+//                System.out.println(output);
+//                System.out.println("Wynik :" + wynik);
+//                break;
+//            case 4:
+//                for (int i = 1; i < ile; i++) {
+//                    wynik /= liczby[i];
+//                    output = output + " / " + liczby[i];
+//                }
+//                System.out.println(output);
+//                System.out.println("Wynik :" + wynik);
+//                break;
+//            default:
+//                System.out.println("Nie podałeś w pierwszym polu liczby z przedziału 1-4");
+//
+//        }
 
         /* weryfikacja, że w tablicy są właściwe watości
 
@@ -87,9 +119,6 @@ public class Main {
         }
 
         */
-
-
-
 
 
     }
